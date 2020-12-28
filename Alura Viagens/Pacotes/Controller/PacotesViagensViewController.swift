ac32d907567ea7fca35f49e5c417838a465e63ae
aloisiofjrdev <aloisiofjr@hotmail.com>
2020-12-28 13:20:46 -0300

    Customizando a tela de pacote de viagens

---
+++ Alura Viagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PacotesViagensViewController.swift
+//  Alura Viagens
+//
+//  Created by Aloisio Formento Junior on 28/12/20.
+//  Copyright © 2020 Aloisio Formento Junior. All rights reserved.
+//
+
+import UIKit
+
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource {
+    
+    @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colecaoPacotesViagem.dataSource = self
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
+        
+        celulaPacote.backgroundColor = UIColor.blue
+        
+        return celulaPacote
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Alura Viagens/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  Alura Viagens
+//
+//  Created by Aloisio Formento Junior on 28/12/20.
+//  Copyright © 2020 Aloisio Formento Junior. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    
+}
