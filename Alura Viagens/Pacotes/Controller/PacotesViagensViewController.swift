0b8aed3f08d2dbdfc072f4b14d22b20fe295f0aa
aloisiofjrdev <aloisiofjr@hotmail.com>
2020-12-29 14:28:22 -0300

    Enviando e recebendo objeto entre controllers

--- Alura Viagens/Pacotes/Controller/PacotesViagensViewController.swift
+++ Alura Viagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -59,9 +59,13 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let pacote = listaViagens[indexPath.item]
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "detalhes") as! DetalhesViagensViewController
+        controller.pacoteSelecionado = pacote
         self.present(controller, animated: true, completion: nil)
+        
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
