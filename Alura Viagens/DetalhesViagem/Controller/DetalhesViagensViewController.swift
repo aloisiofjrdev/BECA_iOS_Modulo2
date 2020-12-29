136c6e3ceb1eab10b9b61ec76fe67738ea2b9fc5
aloisiofjrdev <aloisiofjr@hotmail.com>
2020-12-29 13:20:56 -0300

    Utilizando o protocolo UICollectionViewDelegate

---
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetalhesViagensViewController.swift
+//  Alura Viagens
+//
+//  Created by Aloisio Formento Junior on 29/12/20.
+//  Copyright © 2020 Aloisio Formento Junior. All rights reserved.
+//
+
+import UIKit
+
+class DetalhesViagensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
