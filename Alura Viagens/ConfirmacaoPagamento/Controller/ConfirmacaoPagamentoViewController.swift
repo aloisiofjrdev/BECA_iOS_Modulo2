e474487e6762dfd44f3b982dae93daa13fb73758
aloisiofjrdev <aloisiofjr@hotmail.com>
2020-12-29 22:04:10 -0300

    Inserindo NavigationController no app

--- Alura Viagens/ConfirmacaoPagamento/Controller/ConfirmacaoPagamentoViewController.swift
+++ Alura Viagens/ConfirmacaoPagamento/Controller/ConfirmacaoPagamentoViewController.swift
@@ -16,6 +16,7 @@ class ConfirmacaoPagamentoViewController: UIViewController {
     @IBOutlet weak var labelDataPacoteViagem: UILabel!
     @IBOutlet weak var labelQuantidadePessoas: UILabel!
     @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
+    @IBOutlet weak var botaoVoltarHome: UIButton!
     
     var pacoteComprado: PacoteViagem? = nil
     
@@ -23,21 +24,25 @@ class ConfirmacaoPagamentoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         if let pacote = pacoteComprado {
-            print(pacote.viagem.titulo)
+            self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
+            self.labelTituloPacoteViagem.text = pacote.viagem.titulo.uppercased()
+            self.labelHotelPacoteViagem.text = pacote.nomeDoHotel
+            self.labelDataPacoteViagem.text = pacote.dataViagem
+            self.labelDescricaoPacoteViagem.text = pacote.descricao
+            
+            self.imagemPacoteViagem.layer.cornerRadius = 10
+            self.imagemPacoteViagem.layer.masksToBounds = true
+            
+            self.botaoVoltarHome.layer.cornerRadius = 8
         }
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func botaoVoltarHome(_ sender: UIButton) {
+        if let navigation = self.navigationController {
+            navigation.popToRootViewController(animated: true)
+        }
     }
-    */
-
+    
 }
